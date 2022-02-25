6e743519e85529469d83c5ea344e3ecc737b0f98
srinivasrowdur <99046278+srinivasrowdur@users.noreply.github.com>
2022-02-25 22:23:50 +0000

    Initial Commit

---
+++ StarBuild/StarBuildApp.swift
@@ -0,0 +1,17 @@
+//
+//  StarBuildApp.swift
+//  StarBuild
+//
+//  Created by Srinivas Rowdur on 2/25/22.
+//
+
+import SwiftUI
+
+@main
+struct StarBuildApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
